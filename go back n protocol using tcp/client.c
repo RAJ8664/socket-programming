37c465a730a9098c90f053cf496a60ef46b59f09
Rk Roy <rajr86642@gmail.com>
2025-07-05 17:43:31 +0545

    Update client.c

--- go back n protocol using tcp/client.c
+++ go back n protocol using tcp/client.c
@@ -4,7 +4,6 @@
 #include<unistd.h>
 #include<string.h>
 
-
 int main() {
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -12,14 +11,11 @@ int main() {
     address.sin_family = AF_INET;
     address.sin_port = htons(8080);
     inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr);
-
-
+    
     connect(client_fd, (struct sockaddr*)&address, sizeof(address));
 
     char* msg[] = {"frame1, frame2, frame3"};
-
-
-
+    
     char toreceive[1024] = {0};
     char tosend[1024] = {0};
     for (int i = 0; i < 3; i++) {
@@ -30,7 +26,5 @@ int main() {
         read(client_fd, toreceive, strlen(toreceive));
         printf("Frame ack:%s\n", toreceive);
     }
-
-
     return 0;
 }
